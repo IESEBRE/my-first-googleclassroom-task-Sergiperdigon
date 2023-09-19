083062592e0e1a98ac5ea84a8e6223e97493f2fb
sergiperdigon <sergiperdigon@iesebre.com>
2023-09-19 13:28:47 +0200

    he afegit el primer projecte de java del curs

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    //exemple de comentari d'una sola linea
+    public static void main(String[] args) {
+        //vaij a mostrar per la pantalla el misatge "hello world"
+        //shortcut del System.out.println --> sout
+        System.out.println("Hello world");
+        System.out.println("Segona linie de text");
+        System.out.print("Me dic sergi");
+        System.out.println("i soc alumne de 1'MPO3");
+
+    }
+        /*inci del comentaride mes duna linea
+        System.out.println("Hello world!");
+    }Fi del comentari de mes duna linea*/
+}
